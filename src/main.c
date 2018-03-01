19d5a116ca27568cb73c6350049ffa4b9e940573
John <johnkc1992@gmail.com>
2018-03-01 11:30:06 -0700

    Create structures for the tasks

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+typedef struct {
+	const char* ID;
+	int C;
+	int T;
+} PeriodicTask;
+
+typedef struct {
+	const char* ID;
+	int C;
+	int r;
+} AperiodicTask;
+
 int main(int argc, char** argv) {
 	const char* filein = argv[1];
 	const char* fileout = argv[2];
