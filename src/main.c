582d965558dc92c7ad82ecb7ebd296dbba336613
John <johnkc1992@gmail.com>
2018-03-01 12:58:07 -0700

    naive implementation of parsing for given file format

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
-	const char* ID;
+	char* ID;
 	uint16_t C;
 	uint16_t T;
 } PeriodicTask;
 
 typedef struct {
-	const char* ID;
+	char* ID;
 	uint16_t C;
 	uint16_t r;
 } AperiodicTask;
@@ -28,31 +30,130 @@ void Simulate(Simulation* plan) {
 	
 }
 
+#define BUFF_N 256
 int main(int argc, char** argv) {
 	const char* filein = argv[1];
 	const char* fileout = argv[2];
 	
 	printf("The input file: %s\nThe output file: %s\r\n", filein, fileout);
 	
-	Simulation* plan = calloc(sizeof(Simulation), 1);
+	Simulation* plan = (Simulation*)calloc(sizeof(Simulation), 1);
 	
 	FILE* fin = fopen(filein, "r");
-	// Parse the file to get pCount
-	// Parse the file to get time
 	
-	plan->pTasks = calloc(sizeof(PeriodicTask), plan->pCount);
+	size_t buffsize = 256;
+	char* buff = (char*)calloc(sizeof(char), buffsize);
+	
+	// Parse the file to get pCount
+	fgets(buff, buffsize, fin);
+	plan->pCount = atoi(buff);
+	
+	// Parse the file to get time
+	fgets(buff, buffsize, fin);
+	plan->time = atoi(buff);
+	
+	printf("Time: %i\npCount: %i\n", plan->time, plan->pCount);
+	
+	plan->pTasks = (PeriodicTask*)calloc(sizeof(PeriodicTask), plan->pCount);
+	
+	size_t line_n; // n is the sizeof each read line including 2 for \r\n
+	size_t id_n; // a counter used to get the size of a task id
+	
 	// Parse the file pCount times to get the data for each periodic task
+	for(int i = 0; i < plan->pCount; ++i) {
+		line_n = getline(&buff, &buffsize, fin);
+		printf("buff[%i]: %s", line_n, buff);
+		
+		PeriodicTask* task = (plan->pTasks) + i;
+		
+		// Get the ID
+		int eos = 0; // end of string
+		while (eos < line_n && buff[eos] != ',') { ++eos; }
+		// A fully rigorous program would probably do some validation here
+		// calloc for convenient null terminator
+		// performance implications compared to manually adding \0 after malloc and memcpy unkown
+		task->ID = (char*)calloc(sizeof(char), eos + 1);
+		memcpy(task->ID, buff, eos);
+		
+		// Get the execution time
+		int bos = eos++; // beginning of string
+		while (eos < line_n && buff[eos] == ' ') { ++eos; ++bos; } // Get rid of space
+		while (eos < line_n && buff[eos] != ',') { ++eos; }
+		// A fully rigorous program would probably do some validation here
+		buff[eos] = 0; // create null pointer to aid atoi
+		task->C = atoi(buff + bos);
+		
+		// Get the period
+		bos = eos++;
+		while (eos < line_n && buff[eos] == ' ') { ++eos; ++bos; } // Get rid of space
+		while (eos < line_n && buff[eos] != ',') { ++eos; }
+		// A fully rigorous program would probably do some validation here
+		buff[eos] = 0; // create null pointer to aid atoi
+		task->T = atoi(buff + bos);
+		
+		printf("pTasks[%i]: {ID: \"%s\", C: %i, T: %i}\n", i, task->ID, task->C, task->T);
+	}
 	
 	// Parse the file to get aCount
-	plan->aTasks = calloc(sizeof(AperiodicTask), plan->aCount);
-	// Parse the file aCount times to get the data for each periodic task
+	fgets(buff, buffsize, fin);
+	plan->aCount = atoi(buff);
 	
+	printf("aCount: %i\n", plan->aCount);
+	
+	plan->aTasks = (AperiodicTask*)calloc(sizeof(AperiodicTask), plan->aCount);
+	
+	// Parse the file aCount times to get the data for each periodic task
+	for(int i = 0; i < plan->pCount; ++i) {
+		line_n = getline(&buff, &buffsize, fin);
+		printf("buff[%i]: %s", line_n, buff);
+		
+		AperiodicTask* task = (plan->aTasks) + i;
+		
+		// Get the ID
+		int eos = 0; // end of string
+		while (eos < line_n && buff[eos] != ',') { ++eos; }
+		// A fully rigorous program would probably do some validation here
+		// calloc for convenient null terminator
+		// performance implications compared to manually adding \0 after malloc and memcpy unkown
+		task->ID = (char*)calloc(sizeof(char), eos + 1);
+		memcpy(task->ID, buff, eos);
+		
+		// Get the execution time
+		int bos = eos++; // beginning of string
+		while (eos < line_n && buff[eos] == ' ') { ++eos; ++bos; } // Get rid of space
+		while (eos < line_n && buff[eos] != ',') { ++eos; }
+		// A fully rigorous program would probably do some validation here
+		buff[eos] = 0; // create null pointer to aid atoi
+		task->C = atoi(buff + bos);
+		
+		// Get the absolute release time
+		bos = eos++;
+		while (eos < line_n && buff[eos] == ' ') { ++eos; ++bos; } // Get rid of space
+		while (eos < line_n && buff[eos] != ',') { ++eos; }
+		// A fully rigorous program would probably do some validation here
+		buff[eos] = 0; // create null pointer to aid atoi
+		task->r = atoi(buff + bos);
+		
+		printf("aTasks[%i]: {ID: \"%s\", C: %i, r: %i}\n", i, task->ID, task->C, task->r);
+	}
+	
+	// Cleanup the input file buffer
 	fclose(fin);
 	
+	plan->fout = fopen(fileout, "w");
 	
-	FILE* fout = fopen(fileout, "w");
-	Simulate(periodicTasks, aperiodicTasks, fout);
-	fclose(fout);
+	// Run the simulation
+	Simulate(plan);
+	
+	// Cleanup after ourselves
+	fclose(plan->fout);
+	
+	for (int i = 0; i < plan->pCount; ++i) {
+		free(plan->pTasks[i].ID);
+	}
+	for (int i = 0; i < plan->aCount; ++i) {
+		free(plan->aTasks[i].ID);
+	}
 	
 	free(plan->pTasks);
 	free(plan->aTasks);
