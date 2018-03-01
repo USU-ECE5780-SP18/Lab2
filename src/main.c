0601a8657b30f931df186739dfa9fb481245b36b
John <johnkc1992@gmail.com>
2018-03-01 11:49:08 -0700

    wip

--- src/main.c
+++ src/main.c
@@ -37,10 +37,16 @@ int main(int argc, char** argv) {
 	Simulation* plan = calloc(sizeof(Simulation), 1);
 	
 	FILE* fin = fopen(filein, "r");
-	// Parse the file to get n and contents of each periodic task
-	plan->pTasks = calloc(sizeof(PeriodicTask), n);
-	plan->aTasks = calloc(sizeof(AperiodicTask), n);
-	// periodicTasks[i]
+	// Parse the file to get pCount
+	// Parse the file to get time
+	
+	plan->pTasks = calloc(sizeof(PeriodicTask), plan->pCount);
+	// Parse the file pCount times to get the data for each periodic task
+	
+	// Parse the file to get aCount
+	plan->aTasks = calloc(sizeof(AperiodicTask), plan->aCount);
+	// Parse the file aCount times to get the data for each periodic task
+	
 	fclose(fin);
 	
 	
